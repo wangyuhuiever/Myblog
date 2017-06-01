1050fe1500051914bf2b20ea4da29ec457e5d443
wangyuhuiever <wangyuhuiever@163.com>
2017-06-01 09:52:23 +0800

    second step
    修改主界面

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ bootstrap = Bootstrap()
 def create_app(config_name):
     app = Flask(__name__)
 
+    bootstrap.init_app(app)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess'
+
+    @staticmethod
+    def init_app(app):
+        pass
