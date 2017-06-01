2121222fff5f2acc7860494d239a9a6de531d333
wangyuhuiever <wangyuhuiever@163.com>
2017-06-01 09:32:19 +0800

    first step
    创建所需目录

---
+++ manage.py
@@ -0,0 +1,13 @@
+import os
+from app import create_app
+from flask_script import Manager, Shell
+
+app = create_app(os.getenv('MYBLOG_CONFIG') or 'default')
+manage = Manager(app)
+
+def make_shell_context():
+    return dict(app=app)
+manage.add_command('shell', Shell(make_context=make_shell_context))
+
+if __name__ == '__main__':
+    manage.run()
