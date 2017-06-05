383d56f157b4109db1a9d166741a4d949231b712
wangyuhuiever <wangyuhuiever@163.com>
2017-06-05 11:39:35 +0800

    显示关注者的文章。

--- app/models.py
+++ app/models.py
@@ -115,6 +115,7 @@ class User(UserMixin,db.Model):
                 self.role = Role.query.filter_by(default=True).first()
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        self.follow(self)
 
     def can(self, permissions):
         return self.role is not None and (self.role.permissions & permissions) == permissions
@@ -173,6 +174,19 @@ class User(UserMixin,db.Model):
 
     def is_followed_by(self, user):
         return self.followers.filter_by(followers_id=user.id).first() is not None
+
+    @property
+    def followed_posts(self):
+        return Post.query.join(Follow, Follow.followed_id == Post.author_id).filter(
+            Follow.follower_id == self.id )
+
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
 
     def __repr__(self):
         return '<User %r>' % self.username
